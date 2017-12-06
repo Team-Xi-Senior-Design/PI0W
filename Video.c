8fbd3f11c3e111b5f3143156023ca896db554ac2
pikem <32081532+pikem@users.noreply.github.com>
2017-12-05 19:32:32 -0600

    Outline
    Added outline for Pi0W, helmet side

---
+++ Video.c
@@ -0,0 +1,54 @@
+/* Author: MSOE Team Xi Senior Design 2017
+ * Date: 12/2017
+ * Description: Contains functions that will control video
+ */
+
+ /********************
+ * External Includes *
+ *********************/
+#include "Video.h"
+
+/*
+ * Description:
+ * @param:
+ * @return: NULL
+ */
+void  initVideo(){
+	
+}
+
+/*
+ * Description:
+ * @param:
+ * @return: NULL
+ */
+void  cleanUpVideo(){
+	
+}
+
+/*
+ * Description:
+ * @param:
+ * @return: NULL
+ */
+void  displayData(){
+	
+}
+
+/*
+ * Description:
+ * @param:
+ * @return:
+ */
+void* runVideo(void* run){
+	
+}
+
+/*
+ * Description:
+ * @param:
+ * @return: NULL
+ */
+void  bluetoothRead(){
+
+}
---
+++ main.c
@@ -0,0 +1,17 @@
+/* Author: MSOE Team Xi Senior Design 2017
+ * Date: 12/2017
+ * Description: Main c file for the helmet side controller
+ */
+
+ /********************
+ * External Includes *
+ *********************/
+#include "main.h"
+#include "Audio.h"
+#include "Video.h"
+
+int main(int argc, char* argv[]){
+	
+	printf("Hello\n");
+	return (0);
+}
