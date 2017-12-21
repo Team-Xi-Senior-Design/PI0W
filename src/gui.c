0ccb3ce5234b8741a1049fa26fd35aa78ea7af1c
Reid Rumack <rumackr@msoe.edu>
2017-12-20 19:14:25 -0600

    Int commit
    most functions defined, Not implemented.

---
+++ src/gui.c
@@ -0,0 +1,43 @@
+#include <ncurses.h>
+#include <stdtype.h>
+
+#include <main.h>
+
+
+static uint8_t  micOpen = 1;
+
+static uint8_t  mute    = 0;
+
+void displaySpeedMph(const int32_t speedKph){
+
+
+}
+
+void displaySpeedKph(const int32_t speedKph){
+
+}
+
+void displayFuelLevel(const int32_t levelPersent){
+
+}
+
+void displayRpm(const int32_t rpm){
+
+}
+
+
+void displayPackName(const char* packname){
+
+void displayChatRoomName(const char* roomName){
+
+}
+
+void displayRoomMembers(const char** roomMembers){
+
+}
+
+void setMicIcon(const uint8_t  micState){
+
+}
+
+void setpMuteIcon()
