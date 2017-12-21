8ca308a92b3c8a30308f07fb053e8dad1fcad869
Rumack <rumackr@ad.msoe.edu>
2017-12-20 21:45:55 -0600

    end of day 1 still in inital implemention

--- src/gui.c
+++ src/gui.c
@@ -8,36 +8,43 @@ static uint8_t  micOpen = 1;
 
 static uint8_t  mute    = 0;
 
-void displaySpeedMph(const int32_t speedKph){
+void setConfig(){
+	//set cursor to invisibil
+	curs_set(0);
+	
 
+}
+void dispSpeedMph(const int32_t speedKph){
 
 }
 
-void displaySpeedKph(const int32_t speedKph){
+void dispSpeedKph(const int32_t speedKph){
 
 }
 
-void displayFuelLevel(const int32_t levelPersent){
+void dispFuelLevel(const int32_t levelPersent){
 
 }
 
-void displayRpm(const int32_t rpm){
+void dispRpm(const int32_t rpm){
 
 }
 
+void dispPackName(const char* packname){
 
-void displayPackName(const char* packname){
-
-void displayChatRoomName(const char* roomName){
+}
+void dispChatRoomName(const char* roomName){
 
 }
 
-void displayRoomMembers(const char** roomMembers){
+void dispRoomMembers(const char** roomMembers){
 
 }
 
-void setMicIcon(const uint8_t  micState){
+void setMicIcon(const uint8_t micState){
 
 }
 
-void setpMuteIcon()
+void setSpkrMuteIcon(const uint8_t spkrState){
+
+}
